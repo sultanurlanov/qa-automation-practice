2bd3b68f9b42052c303fde3377396ec0ee829c70
Sultan Nurlanov <your_email@example.com>
2026-09-15 09:34:16 +0600

    Add API test with body validation: JsonPath, hasSize, equalTo

--- src/test/java/io/github/sultanurlanov/api/test/ProductApiTest.java
+++ src/test/java/io/github/sultanurlanov/api/test/ProductApiTest.java
@@ -1,7 +1,9 @@
 package io.github.sultanurlanov.api.test;
 
+import org.checkerframework.checker.units.qual.N;
 import org.testng.annotations.Test;
 import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.*;
 
 public class ProductApiTest {
 
@@ -13,8 +15,17 @@ public class ProductApiTest {
                 .get("https://api.practicesoftwaretesting.com/products")
                          .then()
                          .statusCode(200);
+    }
 
+    @Test
+    public void testProductsBody() {
 
-
+        given()
+                .when()
+                .get("https://api.practicesoftwaretesting.com/products")
+                .then()
+                .statusCode(200)
+                .body("data", hasSize(9))
+                .body("data[0].name",equalTo("Combination Pliers"));
     }
 }
