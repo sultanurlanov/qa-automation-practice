126da17109eb73697a216574c4b434aa9b6d994d
Sultan Nurlanov <your_email@example.com>
2026-09-18 09:16:16 +0600

    commit -m "Add Response handling: extract userId and email, verify password not returned

--- src/test/java/io/github/sultanurlanov/api/test/UserApiTest.java
+++ src/test/java/io/github/sultanurlanov/api/test/UserApiTest.java
@@ -1,5 +1,7 @@
 package io.github.sultanurlanov.api.test;
 
+import io.restassured.response.Response;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 import static io.restassured.RestAssured.given;
 import static org.hamcrest.Matchers.*;
@@ -9,6 +11,8 @@ public class UserApiTest {
     @Test
     public void testRegisterUser() {
 
+        String emailResponse =  "sultan" + System.currentTimeMillis() + "@test.com";
+
         String requestBody = "{"
                 + "\"first_name\": \"Sultan\","
                 + "\"last_name\": \"Nurlanov\","
@@ -16,15 +20,26 @@ public class UserApiTest {
                 + "\"phone\": \"996550667567\","
                 + "\"dob\": \"1994-08-13\","
                 + "\"password\": \"Test12321!\","
-                + "\"email\": \"sultan" + System.currentTimeMillis() + "@test.com\""
+                + "\"email\": \"" + emailResponse + "\""
                 + "}";
 
-        given()
+        Response response  = given()
                 .header("Content-Type","application/json")
                 .body(requestBody)
                 .when()
-                .post("https://api.practicesoftwaretesting.com/users/register")
-                .then()
+                .post("https://api.practicesoftwaretesting.com/users/register");
+
+                response.then()
                 .statusCode(201);
+
+                String userId = response.jsonPath().getString("id");
+                String email = response.jsonPath().getString("email");
+                String password = response.jsonPath().getString("password");
+
+        Assert.assertEquals(email, emailResponse);
+        Assert.assertNull(password);
+
+
+        System.out.println("UserID: " + userId);
     }
 }
